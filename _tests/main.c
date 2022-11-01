c60ab8cd3db6cb7c607081e93f0f9ee782053083
Viet Dinh <54ckb0y789@gmail.com>
2022-10-31 20:59:30 -0400

    Add additional tests

--- _tests/main.c
+++ _tests/main.c
@@ -3,3 +3,21 @@
 void leak() {
   void *stuff = malloc(1000000);
 }
+
+typedef struct {
+  int value;
+} my_t;
+
+void double_free() {
+  my_t *st = malloc(sizeof(my_t));
+  st->value = 1;
+  free(st);
+  // free(st);
+}
+
+void use_after_free() {
+  my_t *st = malloc(sizeof(my_t));
+  st->value = -1;
+  free(st);
+  // st->value = 2;
+}
--- _tests/test/main_test.dart
+++ _tests/test/main_test.dart
@@ -3,9 +3,19 @@ import 'package:test/test.dart';
 
 final dylib = DynamicLibrary.open(Uri.base.resolve('main').toFilePath());
 final leak = dylib.lookupFunction<Void Function(), void Function()>('leak');
+final doubleFree =
+    dylib.lookupFunction<Void Function(), void Function()>('double_free');
+final useAfterFree =
+    dylib.lookupFunction<Void Function(), void Function()>('use_after_free');
 
 void main() {
   test('leak', () {
     leak();
+  });
+  test('double-free', () {
+    doubleFree();
+  });
+  test('use after free', () {
+    useAfterFree();
   });
 }
